478acd051a8d7e238c537254362ae2a9e72783bb
Abdelhady <abdelhadymohammed1763@gmail.com>
2024-08-20 13:10:25 -0700

    print comb v2

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 for i in range(100):
-    print("{:02}, ".format(i), end='')
-    if i == 99:
-        print("{:02}".format(i))
+    print("{:02}, ".format(i), end='') if i < 99 else print("{:02}".format(i))
